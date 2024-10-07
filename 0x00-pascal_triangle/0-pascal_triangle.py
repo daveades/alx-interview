4123b9d8257fe4b0d62abe04e12d56fcdf0cc18c
David Adesanya <jesugbohunmi@outlook.com>
2024-10-07 19:27:28 +0100

    Add module documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Implementation of pascal's triangle
+"""
+
+
 def pascal_triangle(n):
     """
     Given a non-negative integer n, generate the first n rows of Pascal's triangle.
